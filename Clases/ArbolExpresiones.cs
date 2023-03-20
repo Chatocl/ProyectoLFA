e277d03faa315bb0917349333edcbe37d11f9d37
Chatocl <carlos.coronadolazo@gmail.com>
2023-03-20 16:25:39 -0600

    Creacion de los nodos y inicio del arbol

---
+++ Clases/ArbolExpresiones.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clases
+{
+    public class ArbolExpresiones<T>:Nodo<T> where T : IComparable<T>
+    {
+        private Nodo<T> temp = new Nodo<T>();
+    }
+}
---
+++ Clases/Nodo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clases
+{
+    public class Nodo<T> where T : IComparable<T>
+    {
+        public Nodo<T> Izquierdo { get; set; }
+        public Nodo<T> Derecho { get; set; }
+        public T Valor { get; set; }
+        public string First { get; set; }
+        public string Last{ get; set; }
+        public bool Anulable { get; set; }
+    }
+}
